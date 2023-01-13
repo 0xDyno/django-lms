9cb4b44ed56ef56e5466bc93e67a57626fb10dcc
Dyno <0xDyno@gmail.com>
2023-01-14 00:30:09 +0700

    Change ' to ''

--- lms/settings.py
+++ lms/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-_k%d-n@+ka4h(qba%656$fi=oi0anb0jioo4wd$_i&i!w9j8=n"
 
-# SECURITY WARNING: don"t run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
