caa215e12fdeb020cc3bcdbe374aaedab7cdfec7
Dyno <0xDyno@gmail.com>
2023-01-18 17:00:13 +0700

    fixed bug when None passes to ValidateGroupStartDate

--- groups/validators.py
+++ groups/validators.py
@@ -7,8 +7,11 @@ from django.utils.deconstruct import deconstructible
 @deconstructible
 class ValidateGroupStartDate:
     
-    def __init__(self, date=datetime.date.today()):
-        self.date = date
+    def __init__(self, date: datetime.date = None):
+        if date and isinstance(date, datetime.date):
+            self.date = date
+        else:
+            self.date = datetime.date.today()
     
     def __call__(self, *args, **kwargs):
         group_date = args[0]
